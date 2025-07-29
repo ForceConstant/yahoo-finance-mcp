bd883e692ad99d6cc73b5f3409531e33e259b1ae
ForceConstant <sports@forceconstant.com>
2025-07-29 13:24:24 -0400

    Update server.py

--- server.py
+++ server.py
@@ -414,6 +414,4 @@ async def get_recommendations(ticker: str, recommendation_type: str, months_back
 if __name__ == "__main__":
     # Initialize and run the server
     print("Starting Yahoo Finance MCP server...")
-    yfinance_server.run(
-        transport="http"
-    )
+    yfinance_server.run(transport="http")
